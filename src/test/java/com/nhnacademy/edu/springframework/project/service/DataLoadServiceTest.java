f4f1f566825f59074912258019314127badd506f
배수한 <qotngks152@naver.com>
2022-10-14 09:37:10 +0900

    Test 2
    Test폴더에 있는 Score.csv 수정하여 merge 동작하도록 함

--- src/test/java/com/nhnacademy/edu/springframework/project/service/DataLoadServiceTest.java
+++ src/test/java/com/nhnacademy/edu/springframework/project/service/DataLoadServiceTest.java
@@ -1,10 +1,14 @@
 package com.nhnacademy.edu.springframework.project.service;
 
 import com.nhnacademy.edu.springframework.project.repository.CsvScores;
+import com.nhnacademy.edu.springframework.project.repository.CsvStudents;
+import com.nhnacademy.edu.springframework.project.repository.Score;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
 import java.util.InputMismatchException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +17,12 @@ class DataLoadServiceTest {
     @Test
     void loadAndMerge() {
         DataLoadService dataLoadService = new CsvDataLoadService();
-        Assertions.assertThrows(InputMismatchException.class, dataLoadService::loadAndMerge);
+//        Assertions.assertThrows(InputMismatchException.class, dataLoadService::loadAndMerge);
+        dataLoadService.loadAndMerge();
+        List<Score> all = CsvScores.getInstance().findAll();
+        org.assertj.core.api.Assertions.assertThat(all.get(0).getScore()).isEqualTo(30);
+
+        Collection<Student> all1 = CsvStudents.getInstance().findAll();
+        org.assertj.core.api.Assertions.assertThat(all1.isEmpty()).isFalse();
     }
 }
--- src/test/java/com/nhnacademy/edu/springframework/project/service/StudentServiceTest.java
+++ src/test/java/com/nhnacademy/edu/springframework/project/service/StudentServiceTest.java
@@ -5,13 +5,16 @@ import com.nhnacademy.edu.springframework.project.repository.CsvStudents;
 import com.nhnacademy.edu.springframework.project.repository.Score;
 import com.nhnacademy.edu.springframework.project.repository.StudentService;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -19,45 +22,50 @@ import static org.mockito.Mockito.when;
 
 class StudentServiceTest {
     StudentService studentService = new DefaultStudentService();
-//    @BeforeEach
-//    public void setup() {
-//        CsvScores.getInstance().load();
-//        CsvStudents.getInstance().load();
-//
-//    }
-//
-//    @AfterEach
-//    public void finished() {
-//        CsvScores.getInstance().clear();
-//        CsvStudents.getInstance().clear();
-//    }
+    @BeforeEach
+    public void setup() {
+        DataLoadService dataLoadService = new CsvDataLoadService();
+        dataLoadService.loadAndMerge();
+    }
+
+    @AfterEach
+    public void finished() {
+        CsvScores.getInstance().clear();
+        CsvStudents.getInstance().clear();
+    }
     @Test
     void getPassedStudents() {
-//        List<Student> studentList = new ArrayList<>();
-//        studentList.add(new Student(1, "A"));
-//
-//        List<Score> scoreList  = new ArrayList<>();
-//        scoreList.add(new Score(1, 90));
-//
-//        CsvScores csvScores = mock(CsvScores.class);
-//        when(csvScores.findAll()).thenReturn(scoreList);
-//
-//        CsvStudents csvStudents = mock(CsvStudents.class);
-//        when(csvStudents.findAll()).thenReturn(studentList);
-//        csvStudents.merge(csvScores.findAll());
+        Collection<Student> passedStudents = studentService.getPassedStudents();
+        Assertions.assertThat(passedStudents.isEmpty()).isFalse();
+    }
 
-//        CsvStudents.getInstance().merge(CsvScores.getInstance().findAll());
+    @Test
+    void getPassedStudents_contentsCheck() {
+        Collection<Student> passedStudents = studentService.getPassedStudents();
+        List<Student> collect = new ArrayList<>(passedStudents);
 
-        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
-            Collection<Student> passedStudents = studentService.getPassedStudents();
-        });
-//        Assertions.assertThat(passedStudents.isEmpty()).isFalse();
+        Condition<Student> condition =
+                new Condition<>(s-> !s.getScore().isFail(),"pass");
+
+        Assertions.assertThat(collect.get(0)).is(condition);
     }
 
     @Test
     void getStudentsOrderByScore() {
-        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
-            Collection<Student> passedStudents = studentService.getStudentsOrderByScore();
-        });
+        Collection<Student> studentsOrderByScore = studentService.getStudentsOrderByScore();
+        Assertions.assertThat(studentsOrderByScore.isEmpty()).isFalse();
+
+    }
+
+    @Test
+    void getStudentsOrderByScore_contentsCheck() {
+        Collection<Student> studentsOrderByScore = studentService.getStudentsOrderByScore();
+        List<Student> collect = new ArrayList<>(studentsOrderByScore);
+
+//        Condition<Student> condition =
+//                new Condition<>(s-> s.getScore().getScore(),"pass");
+
+        Assertions.assertThat(collect)
+                .isSortedAccordingTo(Comparator.comparing(student -> student.getScore().getScore()));
     }
 }
